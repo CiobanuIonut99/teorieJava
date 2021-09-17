5b571f8246ecd7d6f12dd7c3c6dae71d31808457
Ionut <ciobanuionut99@gmail.com>
2021-09-17 22:11:22 +0300

    first commit, java teorie

---
+++ src/student/Student.java
@@ -0,0 +1,29 @@
+package student;
+
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String name;
+    private int age;
+    private double mark;
+    private int absentNumber;
+    private boolean hasScutire;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
